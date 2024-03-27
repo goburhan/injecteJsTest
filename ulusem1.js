0f5f5c41980d19e0007ee37b4544942cf8960cab
goburhan <goburhann@gmail.com>
2024-03-27 15:55:21 +0300

    gg

--- ulusem1.js
+++ ulusem1.js
@@ -1,6 +1,20 @@
 const element = document.querySelector('.cookiePolicy');
 element.style.display = 'none'; true; 
 
+window.Tawk_API.getStatus();
+
+window.Tawk_API.onLoad = function(){
+    var pageStatus = window.Tawk_API.getStatus();
+
+    if(pageStatus === 'online'){
+        // do something for online
+    }else if(pageStatus === 'away'){
+        //do something for away
+    }else{
+        // do something for offline
+    }
+};
+
 window.Tawk_API.onLoad = function(){
     window.Tawk_API.hideWidget();
 };
