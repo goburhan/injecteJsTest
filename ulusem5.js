ff6dced664cf94350ebce212f9ffb9fc331acc0b
goburhan <goburhann@gmail.com>
2024-04-03 14:11:27 +0300

    d

---
+++ ulusem5.js
@@ -0,0 +1,9 @@
+const element = document.querySelector('.cookiePolicy');
+element.style.display = 'none'; true;
+
+window.Tawk_API.onLoad = function () {
+    window.Tawk_API.hideWidget();
+};
+
+const elementToRemove = document.getElementById('yourElementId');
+elementToRemove.style.display = 'none'; true; 
