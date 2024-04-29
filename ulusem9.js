e000d3ed41e6fd9537c747b490e4eca936d6b653
goburhan <goburhann@gmail.com>
2024-04-29 14:06:11 +0300

    d

---
+++ ulusem9.js
@@ -0,0 +1,30 @@
+const element = document.querySelector('.cookiePolicy');
+element.style.display = 'none'; true;
+
+window.Tawk_API.onLoad = function () {
+    window.Tawk_API.hideWidget();
+};
+
+// const altBasvuru = document.getElementById('ApplicationBottom');
+// altBasvuru.style.display = 'none'; true;
+
+const basvuruButtonu = document.getElementsByClassName('mobilBasvuru');
+for (let i = 0; i < basvuruButtonu.length; i++) {
+    basvuruButtonu[i].style.display = 'none';
+}
+
+const basvuruYapButtonu = document.getElementById('ApplicationBottom');
+if (basvuruYapButtonu) {
+    basvuruYapButtonu.remove()
+}
+const footerElement = document.querySelector('footer');
+if (footerElement) {
+    footerElement.remove();
+}
+
+
+var navbardakiEgitimPortalButtonu = document.querySelector('.nav .btn-primary');
+// Remove the button
+if (navbardakiEgitimPortalButtonu) {
+    navbardakiEgitimPortalButtonu.remove();
+}
